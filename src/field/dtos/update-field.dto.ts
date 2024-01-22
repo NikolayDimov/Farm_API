7e7a6d9d3d8a59f2c85b8768a82641e3237cda82
Nikolay Dimov <dimovv.nikolay@gmail.com>
2024-01-22 18:16:07 +0200

    update-field.dto

--- src/field/dtos/update-field.dto.ts
+++ src/field/dtos/update-field.dto.ts
@@ -1,29 +1,40 @@
+import { OmitType, PartialType } from "@nestjs/swagger";
 import {
   IsNotEmpty,
   IsString,
   Matches,
   IsObject,
   IsUUID,
+  IsOptional,
 } from "class-validator";
 import { MultiPolygon, Polygon } from "geojson";
+import { CreateFieldDto } from "./create-field.dto";
 
-export class UpdateFieldDto {
-  @IsNotEmpty({ message: "Name cannot be empty" })
-  @IsString({ message: "Name must be a string" })
-  @Matches(/^[A-Za-z0-9\s\-]+$/, {
-    message: "Name must contain only letters and numbers",
-  })
-  name: string;
+export class UpdateFieldDto extends PartialType(
+  OmitType(CreateFieldDto, ["farmId"] as const),
+) {}
 
-  @IsNotEmpty({ message: "Polygons cannot be empty" })
-  @IsObject({ message: "Polygons must be a valid GeoJSON object" })
-  boundary: MultiPolygon | Polygon;
+//export class UpdateFieldDto{
+// @IsNotEmpty({ message: "Name cannot be empty" })
+// @IsString({ message: "Name must be a string" })
+// @Matches(/^[A-Za-z0-9\s\-]+$/, {
+//   message: "Name must contain only letters and numbers",
+// })
+// @IsOptional()
+// name: string;
 
-  @IsNotEmpty({ message: "SoilId cannot be empty" })
-  @IsUUID("4", { message: "Invalid SoilId" })
-  soilId: string;
+// @IsNotEmpty({ message: "Polygons cannot be empty" })
+// @IsObject({ message: "Polygons must be a valid GeoJSON object" })
+// @IsOptional()
+// boundary: MultiPolygon | Polygon;
 
-  @IsNotEmpty({ message: "FarmId cannot be empty" })
-  @IsUUID("4", { message: "Invalid FarmId" })
-  farmId: string;
-}
+// @IsNotEmpty({ message: "SoilId cannot be empty" })
+// @IsUUID("4", { message: "Invalid SoilId" })
+// @IsOptional()
+// soilId: string;
+
+// @IsNotEmpty({ message: "FarmId cannot be empty" })
+// @IsUUID("4", { message: "Invalid FarmId" })
+// @IsOptional()
+// farmId: string;
+//}
